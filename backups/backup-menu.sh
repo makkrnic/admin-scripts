347e6a0d0a565ebd8061c5272ad2aaae2e2b191c
Mak Krnic <mak@makkrnic.info>
2014-07-28 17:52:38 +0200

    Fix error handing when directory is not created

--- backups/backup-menu.sh
+++ backups/backup-menu.sh
@@ -7,6 +7,11 @@ source './config-default'
 
 mkdir -p "$BACKUP_LOCAL_DIR"
 
+if [ ! -d "$BACKUP_LOCAL_DIR" ] ; then
+  echo "$BACKUP_LOCAL_DIR is not a directory or does not exist. Giving up."
+  exit -1
+fi
+
 echo "Backing up postgres databases..."
 source ./backup-postgres.sh
 echo "done."
